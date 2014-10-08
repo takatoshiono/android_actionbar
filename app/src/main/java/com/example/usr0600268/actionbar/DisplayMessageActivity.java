5bda7a1de67c6342e49cd20f9e9a558ee87e66e8
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-08 18:15:30 +0900

    Return up by a single level

--- app/src/main/java/com/example/usr0600268/actionbar/DisplayMessageActivity.java
+++ app/src/main/java/com/example/usr0600268/actionbar/DisplayMessageActivity.java
@@ -12,6 +12,8 @@ public class DisplayMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
